5c913691ac2697cc5898edc13bb4ee3bc36c3583
Koen Vendrik <k.vendrik@gmail.com>
2015-10-09 15:40:47 +0200

    Create smoothscroll.directive.js

---
+++ smoothscroll.directive.js
@@ -0,0 +1,31 @@
+(function($){
+	
+	var smoothScroll = {
+
+		init: function(){
+			this._$els = $('[data-smooth-scroll-href]');
+			if(!this._$els[0]) return;
+
+			this._bindEvents();
+		},
+
+		_bindEvents: function(){
+			var self = this;
+			this._$els.click(self._handleClick.bind(self));
+		},
+
+		_handleClick: function(e){
+			var target = e.target,
+				$anchor = $(target).closest('[data-smooth-scroll-href]');
+
+			var targetId = $anchor.data('smooth-scroll-href').replace('#', ''),
+				targetEl = document.getElementById(targetId);
+			
+			Velocity(targetEl, 'scroll', { duration: 500 });
+		}
+
+	};
+
+	smoothScroll.init();
+
+}(jQuery));
